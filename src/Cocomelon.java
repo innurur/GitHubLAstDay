4621bb7a08d432cd2378ecf752bfd347bc779a9f
Inara <inarasalih2015@gmail.com>
2020-11-12 10:22:04 -0600

    new lines of code added

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,6 +1,7 @@
 public class Cocomelon {
     public static void main(String[] args) {
 
-
+        System.out.println("Looks like I started to understand the concept ");
+        System.out.println("Will see what it will be");
     }
 }
