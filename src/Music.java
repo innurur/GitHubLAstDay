07b45352d686fbcaf82c6c2478e9ac0c2e28492b
Inara <inarasalih2015@gmail.com>
2020-11-12 11:07:09 -0600

    finishing the code

--- src/Music.java
+++ src/Music.java
@@ -4,6 +4,8 @@ public class Music {
         System.out.println("Enter the music you are looking for");
         System.out.println("Good choice");
         System.out.println("How would you like to pay?");
-        System.out.println("You total is...");
+        System.out.println("Your total is...");
+        System.out.println("Thank you for visiting us!");
+        System.out.println("Good bye!");
     }
 }
