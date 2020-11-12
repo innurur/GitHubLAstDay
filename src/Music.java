3cb2691b7496ece7709393a0428952e5aa770a71
Inara <inarasalih2015@gmail.com>
2020-11-12 11:05:33 -0600

    music list creating

---
+++ src/Music.java
@@ -0,0 +1,7 @@
+public class Music {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the music you are looking for");
+        System.out.println("Good choice");
+    }
+}
