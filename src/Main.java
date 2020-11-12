e783cb26531414ab2b828d2fe459c9699ad79443
Inara <inarasalih2015@gmail.com>
2020-11-12 09:45:06 -0600

    adding some info

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day");
+        System.out.println("It was helpful");
+        System.out.println("But it was fast");
     }
 }
